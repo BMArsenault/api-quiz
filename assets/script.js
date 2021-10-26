a17dcd768f6b4234cce2aacd931d07281e44e604
Brian <BMArsenault@gmail.com>
2021-10-26 15:09:03 -0700

    added to html and js, as well as text to html

--- assets/script.js
+++ assets/script.js
@@ -3,65 +3,65 @@
 
 // Questions
 
-const question = [
-{
-    question: "Write a question in here",
-    choices: [
-        "Choice number 1",
-        "Choice number 2",
-        "Choice number 3",
-        "Choice number 4",
-    ],
-    answer:  "Choice number 3"
-},
-{
-    question: "Write a question in here",
-    choices: [
-        "Choice number 1",
-        "Choice number 2",
-        "Choice number 3",
-        "Choice number 4",
-    ],
-    answer:  "Choice number 3"
-},
-{
-    question: "Write a question in here",
-    choices: [
-        "Choice number 1",
-        "Choice number 2",
-        "Choice number 3",
-        "Choice number 4",
-    ],
-    answer:  "Choice number 3"
-},
-{
-    question: "Write a question in here",
-    choices: [
-        "Choice number 1",
-        "Choice number 2",
-        "Choice number 3",
-        "Choice number 4",
-    ],
-    answer:  "Choice number 3"
-},
-{
-    question: "Write a question in here",
-    choices: [
-        "Choice number 1",
-        "Choice number 2",
-        "Choice number 3",
-        "Choice number 4",
-    ],
-    answer:  "Choice number 3"
-},
-{
-    question: "Write a question in here",
-    choices: [
-        "Choice number 1",
-        "Choice number 2",
-        "Choice number 3",
-        "Choice number 4",
-    ],
-    answer:  "Choice number 3"
-}
-]
+// const question = [
+// {
+//     question: "Write a question in here",
+//     choices: [
+//         "Choice number 1",
+//         "Choice number 2",
+//         "Choice number 3",
+//         "Choice number 4",
+//     ],
+//     answer:  "Choice number 3"
+// },
+// {
+//     question: "Write a question in here",
+//     choices: [
+//         "Choice number 1",
+//         "Choice number 2",
+//         "Choice number 3",
+//         "Choice number 4",
+//     ],
+//     answer:  "Choice number 3"
+// },
+// {
+//     question: "Write a question in here",
+//     choices: [
+//         "Choice number 1",
+//         "Choice number 2",
+//         "Choice number 3",
+//         "Choice number 4",
+//     ],
+//     answer:  "Choice number 3"
+// },
+// {
+//     question: "Write a question in here",
+//     choices: [
+//         "Choice number 1",
+//         "Choice number 2",
+//         "Choice number 3",
+//         "Choice number 4",
+//     ],
+//     answer:  "Choice number 3"
+// },
+// {
+//     question: "Write a question in here",
+//     choices: [
+//         "Choice number 1",
+//         "Choice number 2",
+//         "Choice number 3",
+//         "Choice number 4",
+//     ],
+//     answer:  "Choice number 3"
+// },
+// {
+//     question: "Write a question in here",
+//     choices: [
+//         "Choice number 1",
+//         "Choice number 2",
+//         "Choice number 3",
+//         "Choice number 4",
+//     ],
+//     answer:  "Choice number 3"
+// }
+// ]
