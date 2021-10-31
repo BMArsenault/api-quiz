f1cb97a8ecd611a742dd7f12dc358374357de886
Brian <BMArsenault@gmail.com>
2021-10-30 23:10:40 -0700

    added const and var as well as questions

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,95 @@
-// VARS
+// VARS & CONST
+
+const question = document.getElementById('questionText');
+const answers = Array.from(document.getElementsByClassName('btn-answer'));
+const correctAnswerBonus = 10;
+// const incorrectAnswer = -:10 seconds
+
+
+
 var startButtonEl = document.querySelector('#btn-game')
 var currentQuestionIndex = 0;
+var score = 0;
+var questionCounter = 0;
+var availableQuestions = [];
+var timer = 60;
+
+// Questions
+
+const questions = [
+{
+    question: "Write a question in here",
+    choices: [
+        "Choice number 1",
+        "Choice number 2",
+        "Choice number 3",
+        "Choice number 4",
+    ],
+    answer:  "Choice number 2"
+},
+{
+    question: "Write a question in here 2",
+    choices: [
+        "Choice number 1",
+        "Choice number 2",
+        "Choice number 3",
+        "Choice number 4",
+    ],
+    answer:  "Choice number 3"
+},
+{
+    question: "Write a question in here 3",
+    choices: [
+        "Choice number 1",
+        "Choice number 2",
+        "Choice number 3",
+        "Choice number 4",
+    ],
+    answer:  "Choice number 1"
+},
+{
+    question: "Write a question in here 4",
+    choices: [
+        "Choice number 1",
+        "Choice number 2",
+        "Choice number 3",
+        "Choice number 4",
+    ],
+    answer:  "Choice number 4"
+},
+{
+    question: "Write a question in here 5",
+    choices: [
+        "Choice number 1",
+        "Choice number 2",
+        "Choice number 3",
+        "Choice number 4",
+    ],
+    answer:  "Choice number 2"
+},
+{
+    question: "Write a question in here 6",
+    choices: [
+        "Choice number 1",
+        "Choice number 2",
+        "Choice number 3",
+        "Choice number 4",
+    ],
+    answer:  "Choice number 2"
+}
+]
+
+
+// start game
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    console.log(availableQuestions);
+    timer = 60;
+};
+
 
 // Funcs
 function creatCurrentQuestion() {
@@ -45,70 +134,4 @@ startButtonEl.addEventListener('click', handleClick);
 //             console.log("Time is up!");
 //         }
 // }
-
-
-
-// Questions
-
-// const question = [
-// {
-//     question: "Write a question in here",
-//     choices: [
-//         "Choice number 1",
-//         "Choice number 2",
-//         "Choice number 3",
-//         "Choice number 4",
-//     ],
-//     answer:  "Choice number 3"
-// },
-// {
-//     question: "Write a question in here",
-//     choices: [
-//         "Choice number 1",
-//         "Choice number 2",
-//         "Choice number 3",
-//         "Choice number 4",
-//     ],
-//     answer:  "Choice number 3"
-// },
-// {
-//     question: "Write a question in here",
-//     choices: [
-//         "Choice number 1",
-//         "Choice number 2",
-//         "Choice number 3",
-//         "Choice number 4",
-//     ],
-//     answer:  "Choice number 3"
-// },
-// {
-//     question: "Write a question in here",
-//     choices: [
-//         "Choice number 1",
-//         "Choice number 2",
-//         "Choice number 3",
-//         "Choice number 4",
-//     ],
-//     answer:  "Choice number 3"
-// },
-// {
-//     question: "Write a question in here",
-//     choices: [
-//         "Choice number 1",
-//         "Choice number 2",
-//         "Choice number 3",
-//         "Choice number 4",
-//     ],
-//     answer:  "Choice number 3"
-// },
-// {
-//     question: "Write a question in here",
-//     choices: [
-//         "Choice number 1",
-//         "Choice number 2",
-//         "Choice number 3",
-//         "Choice number 4",
-//     ],
-//     answer:  "Choice number 3"
-// }
-// ]
+startGame ();
